f89169274661ea1b378db759888539480f0d6432
Micah <micahmrocks@gmail.com>
2018-06-30 13:02:23 -0600

    cleaned up the code a bit

--- app/index.js
+++ app/index.js
@@ -14,34 +14,6 @@ class App extends React.Component {
   }
 }
 
-{AddTen(2,3,4)}
-function AddTen(n1,n2,n3) {
-  var numbers = [n1,n2,n3];
-  var numbersPlusTen = numbers.map(function (num) {
-    return num + 10;
-  });
-  console.log(numbersPlusTen) // [11, 12, 13]
-}
-
-{ListFriends('Ean Hi', 'Murphy Hi', 'Merrick Hi')}
-function ListFriends(f1,f2,f3){
-  var friends = [f1,f2,f3];
-  var listItems = friends.map(function(friend){
-    return "<li> " + friend + "</li>";
-  });
-  console.log(listItems); // ["<li> Ean Platter </li>", "<li> Murphy Randall</li>", "<li> Merrick Christensen </li>"];
-}
-
-{FilterFriends('Ean', 'Tyler', 'Mikenzi', 'Eric', 'Jessica')}
-function FilterFriends(f1,f2,f3,f4,f5){
-  var friends = [f1,f2,f3,f4,f5];
-  var newFriends = friends.filter(function (friend) {
-    return friend[0] === 'E'
-  });
-  console.log(newFriends) // ['Ean', 'Eric']
-}
-
-
 ReactDOM.render(
   <App />,
   document.getElementById('app')
