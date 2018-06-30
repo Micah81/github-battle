7dd3f89e405a4a2cd337f56f0fae2253bd514a45
Micah <micahmrocks@gmail.com>
2018-06-30 13:13:27 -0600

    Exported / imported App module

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+// state
+// lifecycle event
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
+
+module.exports = App;
