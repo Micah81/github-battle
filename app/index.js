d76509a5bf4e2f469a8a1fa713d8d65c05b1915b
Micah <micahmrocks@gmail.com>
2018-07-01 13:10:57 -0600

    Converted stateless component in app code to be a stateless functional component.

--- app/index.js
+++ app/index.js
@@ -1,27 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-//var App = require('./components/App');
+var App = require('./components/App');
 
 
-class HelloWorld extends React.Component{
-  render() {
-    return (
-      <div>Hello {this.props.name}</div>
-    )
-  }
-}
-
-
-// The above code can be converted to this code below.
-// This is because the code above has only a render method.
-
-function HelloWorld (props) {
-  return (
-    <div>Hello {props.name}!</div>
-  )
-}
-
-
-// And the two codes above are called in the exact same way:
-ReactDOM.render(<HelloWorld name='Micah' />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
